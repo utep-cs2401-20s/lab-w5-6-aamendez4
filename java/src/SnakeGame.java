6849e1c33fe251e7eb9af5f7f750bfc0b9ef745a
aamendez4 <aamendez4@utep.edu>
2020-02-26 14:44:30 -0700

    - added recursiveChecks methods - added resetCounters method - added getter methods - created SnakeGameTester.java

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -11,45 +11,108 @@ public class SnakeGame {
     public SnakeGame(boolean[][] arr, int x, int y) {
         headPosition[0] = x;
         headPosition[1] = y;
-        game = new boolean[][];
+
+        int size = arr.length;
+        game = new boolean[size][size];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++){
+                game[i][j] = arr[i][j];
+            }
+        }
     }
 
     public int[] findTailExhaustive(boolean[][] game){
         //reset counter first
+        resetCounters();
+
         int count = 0;
         int neighborCount = 0;
+        int[] tailPosition = new int[3];
 
         for (int i = 0; i < game.length; i++) {
             for (int j = 0; j < game[i].length; j++) {
                 exhaustiveChecks++;
-                if (game[i][j] == true){
+                if (game[i][j]){
                     count++;
                     // up
-                    if((game[i - 1][j] == true) && (i - 1 > 0)) {
+                    if((game[i - 1][j]) && (i - 1 > 0)) {
                         neighborCount++;
                     }
                     //down
-                    if((game[i + 1][j] == true) && (i + 1 < game[i].length)) {
+                    if((game[i + 1][j]) && (i + 1 < game[i].length)) {
                         neighborCount++;
                     }
                     //left
-                    if((game[i][j - 1] == true) && (j - 1 > 0)) {
+                    if((game[i][j - 1]) && (j - 1 > 0)) {
                         neighborCount++;
                     }
                     //right
-                    if((game[i][j + 1] == true) && (j + 1 > game[i].length)) {
+                    if((game[i][j + 1]) && (j + 1 > game[i].length)) {
                         neighborCount++;
                     }
 
                     if((neighborCount == 1) && ((i != headPosition[0]) && (j != headPosition[1]))) {
-                        int[] tailPosition = new int[3];
-                        tailPosition[1] = i;
-                        tailPosition[2] = j;
-                        tailPosition[3] = count;
+                        tailPosition[0] = i;
+                        tailPosition[1] = j;
+                        tailPosition[2] = count;
                         return tailPosition;
                     }
                 }
             }
         }
+        return tailPosition;
+    }
+
+    public int[] findTailRecursive(){
+        resetCounters();
+
+        return findTailRecursive(headPosition, headPosition);
+    }
+
+    private int[] findTailRecursive(int[] currentPosition, int[] previousPosition){
+        int count = 0;
+        int neighborCount = 0;
+        int[] tailPosition = new int[3];
+
+        // Base Case
+        // up
+        if((game[currentPosition[0] - 1][currentPosition[1]]) && (currentPosition[0] - 1 > 0)) {
+            neighborCount++;
+        }
+        //down
+        if((game[currentPosition[0] + 1][currentPosition[1]]) && (currentPosition[0] + 1 < game[currentPosition[0]].length)) {
+            neighborCount++;
+        }
+        //left
+        if((game[currentPosition[0]][currentPosition[1] - 1]) && (currentPosition[1] - 1 > 0)) {
+            neighborCount++;
+        }
+        //right
+        if((game[currentPosition[0]][currentPosition[1] + 1]) && (currentPosition[1] + 1 > game[currentPosition[0]].length)) {
+            neighborCount++;
+        }
+
+        if((neighborCount == 1) && ((currentPosition[0] != headPosition[0]) && (currentPosition[1] != headPosition[1]))) {
+            tailPosition[0] = currentPosition[0];
+            tailPosition[1] = currentPosition[1];
+            tailPosition[2] = count;
+            return tailPosition;
+        }
+
+        // Recursive Call
+
+    }
+
+    private void resetCounters(){
+        exhaustiveChecks = 0;
+        recursiveChecks = 0;
+    }
+
+    private static int getRecursiveChecks(){
+        return recursiveChecks;
+    }
+
+    private static int getExhaustiveChecks(){
+        return exhaustiveChecks;
     }
 }
